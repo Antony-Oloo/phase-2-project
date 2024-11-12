e333db7a13b4f1d38dfbe2c2dd66d1e394e409bc
antony <antoloo254@gmail.com>
2024-11-12 14:16:44 +0300

    changes made to About.js

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>This is a coupon discount app where you can view and add coupon codes!</p>
+    </div>
+  );
+}
+
+export default About;
