26a960b1ade031c52b15da3694756d1df20ec24c
antony <antoloo254@gmail.com>
2024-11-12 15:37:26 +0300

    server install and scripts edited

--- src/components/About.js
+++ src/components/About.js
@@ -4,8 +4,10 @@ function About() {
   return (
     <div>
       <h2>About</h2>
-      <p>This is a coupon discount app where you can view and add coupon codes!</p>
-    </div>
+      <p>This is a coupon discount app. The app basically makes it possible for users to view availlable copons from different brands across the country and mostly their favourite ones!</p>
+      <p>Diffent brands also are able to make know to the public the various discounts that they are offering</p>
+      <p>If you are a MARINE then YOU will get more 😂</p>    
+      </div>
   );
 }
 
