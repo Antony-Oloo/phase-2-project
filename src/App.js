9073a0c3f6df0841a55bce8d942dd29f2480963f
antony <antoloo254@gmail.com>
2024-11-12 14:08:02 +0300

    initial commit + App.js modified

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import CouponList from './components/CouponList';
+import CouponForm from './components/CouponForm';
+import CouponDetail from './components/CouponDetail';
+import About from './components/About';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<CouponList />} /> |
+          <Route path="/add" element={<CouponForm />} /> |
+          <Route path="/coupons/:id" element={<CouponDetail />} /> |
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
