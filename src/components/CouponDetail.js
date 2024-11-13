ec5c38d32494caab7591ed98a21ed2245697aad1
antony <antoloo254@gmail.com>
2024-11-13 14:15:45 +0300

    changes made to Navbar.js

--- src/components/CouponDetail.js
+++ src/components/CouponDetail.js
@@ -4,15 +4,27 @@ import { useParams } from 'react-router-dom';
 function CouponDetail() {
   const { id } = useParams();
   const [coupon, setCoupon] = useState(null);
-
+  const [error, setError] = useState(null);
+  
   useEffect(() => {
     fetch(`http://localhost:5000/coupons/${id}`)
-      .then(res => res.json())
-      .then(data => setCoupon(data));
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(`Failed to fetch coupon: ${res.statusText}`);
+        }
+        return res.json();
+      })
+      .then(data => setCoupon(data))
+      .catch(err => setError(err.message));
   }, [id]);
 
+  // If there's an error, show it
+  if (error) return <div>Error: {error}</div>;
+
+  // Show loading message if coupon data is not yet fetched
   if (!coupon) return <div>Loading...</div>;
 
+  // Show coupon details once fetched
   return (
     <div>
       <h3>{coupon.code}</h3>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,23 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      <Link to="/add">Add Coupon</Link>
-      <Link to="/about">About</Link>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="logo">
+          {/* <Link to="/">CouponApp</Link> */}
+        </div>
+        <ul className="nav-links">
+          <li>
+            <Link to="/" className="nav-link">Home</Link>
+          </li>
+          <li>
+            <Link to="/add" className="nav-link">Add Coupon</Link>
+          </li>
+          <li>
+            <Link to="/about" className="nav-link">About</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
