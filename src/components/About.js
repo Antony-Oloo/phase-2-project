10b79087486c36874db58abeb81dd4e03c6d7684
antony <antoloo254@gmail.com>
2024-11-14 23:06:01 +0300

    changes made to the about section

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About() {
     <div className="about-page">
       <div className="about-page-container">
         <h2>About</h2>
-        <p>Our app empowers users to discover a wide variety of exclusive coupons and discounts from their favorite brands, all in one convenient place. With access to the latest promotions nationwide, users can easily find the best deals tailored to their interests.!</p>
+        <p>Our app is meant to empower users by making them discover a wide variety of exclusive coupons and discounts from their favorite brands, all in one convenient place. With access to the latest promotions nationwide, users can easily find the best deals tailored to their interests.!</p>
         <p>For brands, our platform offers a powerful way to reach a broad audience and showcase their current offers, driving engagement and boosting sales..</p>
         <p>If you are a MARINE, then YOU will get more 😂</p>
       </div>
